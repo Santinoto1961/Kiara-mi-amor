29c7435d67ea7291a2f5419345f01596c08db4ac
Santinoto el De la moto <santinocinti@gmail.com>
2025-11-21 12:28:10 -0300

    6meses arreglado

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://Santimoto1961.github.io/Kiara-mi-amor",
+  plugins: [react()]
 });
